bed2cad4d180f83e97f890fe51d532f5e6cce6c8
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-05-07 17:27:03 -0700

    Add feature of Simple Routing

--- UI/src/app.jsx
+++ UI/src/app.jsx
@@ -3,10 +3,16 @@ import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IssueList from './IssueList.jsx';
+import { HashRouter as Router } from 'react-router-dom';
+import Page from './Page.jsx';  
 
 const contentNode = document.getElementById('contents');
-ReactDOM.render(<IssueList />, contentNode); // Render the component inside the content Node
+const element = (
+    <Router>
+        <Page/>
+    </Router>
+)
+ReactDOM.render(element, contentNode); // Render the component inside the content Node
 if (module.hot) {
     module.hot.accept();
     }
