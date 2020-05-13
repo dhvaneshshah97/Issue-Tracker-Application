b09fe386acafae87d83162b8611b3267f3b6cba6
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-05-13 12:27:17 -0700

    Replace HashRouter with Browser History Router

--- UI/uiserver.js
+++ UI/uiserver.js
@@ -1,6 +1,7 @@
- /* eslint linebreak-style: ["error", "windows"] */
+/* eslint linebreak-style: ["error", "windows"] */
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 
 const port = process.env.UI_SERVER_PORT || 8000;
 // const proxy = require('http-proxy-middleware');
@@ -8,7 +9,7 @@ const port = process.env.UI_SERVER_PORT || 8000;
 // if (apiProxyTarget){
 //     app.use('/graphql',proxy({target : apiProxyTarget}));
 // }
-const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT 
+const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT
     || 'http://localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
 
@@ -16,24 +17,27 @@ const app = express();
 
 const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
 if (enableHMR && (process.env.NODE_ENV !== 'production')) {
-console.log('Adding dev middleware, enabling HMR');
-/* eslint "global-require": "off" */
-/* eslint "import/no-extraneous-dependencies": "off" */
-const webpack = require('webpack');
-const devMiddleware = require('webpack-dev-middleware');
-const hotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config.js');
-config.entry.app.push('webpack-hot-middleware/client');
-config.plugins = config.plugins || [];
-config.plugins.push(new webpack.HotModuleReplacementPlugin());
-const compiler = webpack(config);
-app.use(devMiddleware(compiler));
-app.use(hotMiddleware(compiler));
+    console.log('Adding dev middleware, enabling HMR');
+    /* eslint "global-require": "off" */
+    /* eslint "import/no-extraneous-dependencies": "off" */
+    const webpack = require('webpack');
+    const devMiddleware = require('webpack-dev-middleware');
+    const hotMiddleware = require('webpack-hot-middleware');
+    const config = require('./webpack.config.js');
+    config.entry.app.push('webpack-hot-middleware/client');
+    config.plugins = config.plugins || [];
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    const compiler = webpack(config);
+    app.use(devMiddleware(compiler));
+    app.use(hotMiddleware(compiler));
 }
 
 app.get('/env.js', (req, res) => {
     res.send(`window.ENV = ${JSON.stringify(env)}`)
-    })
-const middleware = express.static('public');   
-app.use(middleware);
-app.listen(8000, () => {console.log(`UI started on port ${port}`)});
+})
+const middleware = express.static('public');
+app.use('/', middleware);
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'));
+});
+app.listen(8000, () => { console.log(`UI started on port ${port}`) });
