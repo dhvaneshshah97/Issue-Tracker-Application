57c012d91122fae36af65456a9003a684eeeddbe
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-05-15 16:59:33 -0700

    Add apply and reset

--- UI/src/IssueFilter.jsx
+++ UI/src/IssueFilter.jsx
@@ -1,35 +1,66 @@
 /* eslint linebreak-style: ["error", "windows"] */
 
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom';
+import URLSearchParams from 'url-search-params';
 
 
 // eslint-disable-next-line react/prefer-stateless-function
 class IssueFilter extends React.Component {
-    constructor() {
+    constructor({ location: { search } }) {
         super();
+        const params = new URLSearchParams(search);
+        this.state = {
+            status: params.get('status') || '',
+            changed: false,
+        };
         this.onChangeStatus = this.onChangeStatus.bind(this);
+        this.applyFilter = this.applyFilter.bind(this);
+        this.showOriginalFilter = this.showOriginalFilter.bind(this);
     }
-
+    componentDidUpdate(prevProps) {
+        const { location: { search: prevSearch } } = prevProps;
+        const { location: { search } } = this.props;
+        if (prevSearch !== search) {
+            this.showOriginalFilter();
+        }
+    }
     onChangeStatus(e) {
-        const status = e.target.value;
-        this.props.history.push({
+        this.setState({ status: e.target.value, changed: true });
+    }
+    applyFilter() {
+        const { status } = this.state;
+        const { history } = this.props;
+        history.push({
             pathname: '/issues',
             search: status ? `?status=${status}` : '',
         });
     }
+    showOriginalFilter() {
+        const { location: { search } } = this.props;
+        const params = new URLSearchParams(search);
+        this.setState({
+            status: params.get('status') || '',
+            changed: false,
+        });
+    }
     render() {
+        const { status, changed } = this.state;
         return (
             <div>
                 Status:
                 {' '}
-                <select onChange={this.onChangeStatus}>
+                <select value={status} onChange={this.onChangeStatus}>
                     <option value="">All</option>
                     <option value="New">New</option>
                     <option value="Assigned">Assigned</option>
                     <option value="Fixed">Fixed</option>
                     <option value="Closed">Closed</option>
                 </select>
+                {' '}
+                <button type="button" onClick={this.applyFilter}>Apply</button>
+                {' '}
+                <button type="button" onClick={this.showOriginalFilter} disabled={!changed}>Reset</button>
             </div>
         );
     };
