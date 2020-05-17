4a884cff0556768589c4e94cad9eee810a8c133e
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-05-17 12:26:04 -0700

    Add Filter by effort field

--- UI/src/IssueFilter.jsx
+++ UI/src/IssueFilter.jsx
@@ -11,11 +11,15 @@ class IssueFilter extends React.Component {
         const params = new URLSearchParams(search);
         this.state = {
             status: params.get('status') || '',
+            effortMin: params.get('effortMin') || '',
+            effortMax: params.get('effortMax') || '',
             changed: false,
         };
         this.onChangeStatus = this.onChangeStatus.bind(this);
         this.applyFilter = this.applyFilter.bind(this);
         this.showOriginalFilter = this.showOriginalFilter.bind(this);
+        this.onChangeEffortMin = this.onChangeEffortMin.bind(this);
+        this.onChangeEffortMax = this.onChangeEffortMax.bind(this);
     }
     componentDidUpdate(prevProps) {
         const { location: { search: prevSearch } } = prevProps;
@@ -29,22 +33,42 @@ class IssueFilter extends React.Component {
         const params = new URLSearchParams(search);
         this.setState({
             status: params.get('status') || '',
+            effortMin: params.get('effortMin') || '',
+            effortMax: params.get('effortMax') || '',
             changed: false,
         });
+    }
+    onChangeEffortMin(e) {
+        const effortString = e.target.value;
+        if (effortString.match(/^\d*$/)) {
+            this.setState({ effortMin: e.target.value, changed: true });
+        }
+    }
+    onChangeEffortMax(e) {
+        const effortString = e.target.value;
+        if (effortString.match(/^\d*$/)) {
+            this.setState({ effortMax: e.target.value, changed: true });
+        }
     }
 
     onChangeStatus(e) {
         this.setState({ status: e.target.value, changed: true })
     }
     applyFilter() {
-        const { status } = this.state;
-        this.props.history.push({
-            pathname: '/issues',
-            search: status ? `?status=${status}` : '',
-        });
+        const { status, effortMin, effortMax } = this.state;
+        const { history } = this.props;
+
+        const params = new URLSearchParams();
+        if (status) params.set('status', status);
+        if (effortMin) params.set('effortMin', effortMin);
+        if (effortMax) params.set('effortMax', effortMax);
+        const search = params.toString() ? `?${params.toString()}` : '';
+        history.push({ pathname: '/issues', search });
+
     }
     render() {
         const { status, changed } = this.state;
+        const { effortMin, effortMax } = this.state;
         return (
             <div>
                 Status:
@@ -57,6 +81,20 @@ class IssueFilter extends React.Component {
                     <option value="Closed">Closed</option>
                 </select>
                 {' '}
+                Effort between:
+                {' '}
+                <input
+                    size={5}
+                    value={effortMin}
+                    onChange={this.onChangeEffortMin}
+                />
+                {' - '}
+                <input
+                    size={5}
+                    value={effortMax}
+                    onChange={this.onChangeEffortMax}
+                />
+                {'  '}
                 <button type="button" onClick={this.applyFilter}>Apply</button>
                 {' '}
                 <button type="button" onClick={this.showOriginalFilter} disabled={!changed}>Reset</button>
--- UI/src/IssueList.jsx
+++ UI/src/IssueList.jsx
@@ -30,14 +30,26 @@ export default class IssueList extends React.Component {
         const vars = {};
         if (params.get('status')) vars.status = params.get('status');
 
-        const query = `
-        query issueList($status: StatusType){
-            issueList(status:$status){
+        const effortMin = parseInt(params.get('effortMin'), 10);
+        if (!Number.isNaN(effortMin)) vars.effortMin = effortMin;
+        const effortMax = parseInt(params.get('effortMax'), 10);
+        if (!Number.isNaN(effortMax)) vars.effortMax = effortMax;
+
+        const query = `query issueList(
+            $status: StatusType
+            $effortMin: Int
+            $effortMax: Int
+            ){
+            issueList(
+                status: $status
+                effortMin: $effortMin
+                effortMax: $effortMax 
+                ){
                 id title status owner
                 created effort completionDate 
             }
         }`;
-        
+
 
         const data = await graphQLFetch(query, vars);
         // console.log(data.issueList);
