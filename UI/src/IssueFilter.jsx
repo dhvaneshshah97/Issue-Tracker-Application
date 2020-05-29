47c18ac3936185f9824ed6ea8614e9127aa8786d
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-05-29 14:13:47 -0700

    Add Grid System to Form component of IssueFilter component

--- UI/src/IssueFilter.jsx
+++ UI/src/IssueFilter.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import {  withRouter } from 'react-router-dom'
 import {
-    ButtonToolbar, Button, FormGroup, FormControl, ControlLabel, InputGroup,
+    ButtonToolbar, Button, FormGroup, FormControl, ControlLabel, InputGroup, Row, Col
 } from 'react-bootstrap';
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -72,33 +72,42 @@ class IssueFilter extends React.Component {
         const { status, changed } = this.state;
         const { effortMin, effortMax } = this.state;
         return (
-            <div>
+            <Row>
                 {/* here ControlLabel works as label component, and FormControl works as select component because of componentClass property, otherwise bydefault it works as input component if componentClass property is not specified */}
-                <FormGroup>
-                    <ControlLabel>Status:</ControlLabel>
-                    <FormControl componentClass = "select" value={status} onChange={this.onChangeStatus}>
-                        <option value="">All</option>
-                        <option value="New">New</option>
-                        <option value="Assigned">Assigned</option>
-                        <option value="Fixed">Fixed</option>
-                        <option value="Closed">Closed</option>
-                    </FormControl>
-                </FormGroup>
-                <FormGroup>
-                    <ControlLabel>Effort between:</ControlLabel>
-                    <InputGroup>
-                        <FormControl value={effortMin} onChange={this.onChangeEffortMin} />
-                        {/* The InputGroup.Addon component can be used to display the inputs next to each other, as well as
-show the dash between the two inputs. */}
-                        <InputGroup.Addon>-</InputGroup.Addon>
-                        <FormControl value={effortMax} onChange={this.onChangeEffortMax} />
-                    </InputGroup>
-                </FormGroup>
-                <ButtonToolbar>
-                    <Button type="button" bsStyle="primary" onClick={this.applyFilter}>Apply</Button>
-                    <Button type="button" onClick={this.showOriginalFilter} disabled={!changed}>Reset</Button>
-                </ButtonToolbar>
-            </div>
+                <Col xs={6} sm={4} md={3} lg={2}>
+                    <FormGroup bsSize="small">
+                        <ControlLabel>Status:</ControlLabel>
+                        <FormControl componentClass = "select" value={status} onChange={this.onChangeStatus}>
+                            <option value="">All</option>
+                            <option value="New">New</option>
+                            <option value="Assigned">Assigned</option>
+                            <option value="Fixed">Fixed</option>
+                            <option value="Closed">Closed</option>
+                        </FormControl>
+                    </FormGroup>
+                </Col>
+                <Col xs={6} sm={4} md={3} lg={2}>
+                    <FormGroup bsSize="small">
+                        <ControlLabel>Effort between:</ControlLabel>
+                        <InputGroup>
+                            <FormControl value={effortMin} onChange={this.onChangeEffortMin} />
+                            {/* The InputGroup.Addon component can be used to display the inputs next to each other, as well as
+    show the dash between the two inputs. */}
+                            <InputGroup.Addon>-</InputGroup.Addon>
+                            <FormControl value={effortMax} onChange={this.onChangeEffortMax} />
+                        </InputGroup>
+                    </FormGroup>
+                </Col>
+                <Col xs={6} sm={4} md={3} lg={2}>
+                    <FormGroup>
+                        <ControlLabel>&nbsp;</ControlLabel>
+                        <ButtonToolbar>
+                            <Button type="button" bsStyle="primary" onClick={this.applyFilter} bsSize="small">Apply</Button>
+                            <Button type="button" onClick={this.showOriginalFilter} disabled={!changed} bsSize="small">Reset</Button>
+                        </ButtonToolbar>
+                    </FormGroup>
+                </Col>
+            </Row>
         );
     };
 };
